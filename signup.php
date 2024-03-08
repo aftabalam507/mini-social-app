70fb4fe20daa7f5a9bc407507ab55df55bd176e6
aftabalam507 <aftabalam1996jan@gmail.com>
2024-03-08 20:44:22 +0530

    signup working

--- signup.php
+++ signup.php
@@ -2,7 +2,16 @@
 require "functions.php";
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-  print_r($_POST);
+  $username = addslashes($_POST['username']);
+  $email = addslashes($_POST['email']);
+  $password = addslashes($_POST['password']);
+  $date = date('Y-m-d H:i:s');
+  $query = "insert into users (username,email,password,date) values ('$username','$email','$password','$date')";
+
+  $result = mysqli_query($con, $query);
+
+  header("Location: login.php");
+  die;
 }
 ?>
 
@@ -19,9 +28,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <div style="margin: auto; max-width: 600px;">
     <h2 style="text-align: center;">Signup</h2>
     <form method="post" style="margin:auto; padding:10px;">
-      <input type="text" name="username" placeholder="Username"><br>
-      <input type="text" name="email" placeholder="Email"><br>
-      <input type="text" name="password" placeholder="Password"><br>
+      <input type="text" name="username" placeholder="Username" required><br>
+      <input type="email" name="email" placeholder="Email" required><br>
+      <input type="text" name="password" placeholder="Password" required><br>
       <button class="my-btn">Signup</button>
     </form>
   </div>
